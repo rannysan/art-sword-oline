ac22c99bcbd1484a20794cf600eb4dd487a6f34e
Gabrielbemo <gabrielbemoura@gmail.com>
2020-03-09 01:02:35 -0300

    Adicionando config inicial do lancer

---
+++ art-sword-online/Assets/Player/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float speed = 0.04f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            transform.position += new Vector3(0, speed, 0);
+        }
+    }
+}
