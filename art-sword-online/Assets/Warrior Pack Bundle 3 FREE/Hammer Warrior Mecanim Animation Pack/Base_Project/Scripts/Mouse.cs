a35770b31851eccdcd076e212776d3a09e85030e
joao matheus bueno <jmatheus_05@hotmail.com>
2020-03-11 13:13:33 -0300

    Adicionando comandos basicos no Hammer

---
+++ art-sword-online/Assets/Warrior Pack Bundle 3 FREE/Hammer Warrior Mecanim Animation Pack/Base_Project/Scripts/Mouse.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            print("M0");
+        }
+
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            print("M1");
+        }
+    }
+}
