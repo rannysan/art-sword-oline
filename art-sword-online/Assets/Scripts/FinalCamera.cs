a6abc7719b5e4ed9053fab8e8c82f212c9f50da6
Gabrielbemo <gabrielbemoura@gmail.com>
2020-03-11 23:36:59 -0300

    movimentação 3d

---
+++ art-sword-online/Assets/Scripts/FinalCamera.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalCamera : MonoBehaviour {
+
+	public bool lockCursor;
+	public float mouseSensitivity;
+	public Transform target;
+    private Transform target2;
+    public float dsFromTarget = 2;
+	public Vector2 pitchMinMax = new Vector2 (-40, 85);
+
+	public float rotationSmoothTime = 0.12f;
+	Vector3 rotationSmoothVelocity;
+	Vector3 currentRotation;
+
+	float yaw;
+	float pitch;
+
+    int zoom = 5;
+    int normal = 90;
+    float smooth = 5;
+
+    private bool isZoomed = false;
+
+	void Start()
+	{
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            isZoomed = !isZoomed;
+        }
+
+        if (isZoomed)
+        {
+            GetComponent<Camera>().fieldOfView = Mathf.Lerp(GetComponent<Camera>().fieldOfView, zoom, Time.deltaTime * smooth);
+            //target.transform.position += Vector3.right * Time.deltaTime;
+            transform.position = target2.position - transform.forward * dsFromTarget;
+            mouseSensitivity = 1;
+        }
+
+        else
+        {
+            GetComponent<Camera>().fieldOfView = Mathf.Lerp(GetComponent<Camera>().fieldOfView, normal, Time.deltaTime * smooth);
+            mouseSensitivity = 3;
+        }
+        
+    }
+
+    void LateUpdate () {
+		yaw += Input.GetAxis ("RightStickVertical") * mouseSensitivity;
+		pitch -= Input.GetAxis ("RightStickHorizontal") * mouseSensitivity;
+		yaw += Input.GetAxis ("Mouse X") * mouseSensitivity;
+		pitch -= Input.GetAxis ("Mouse Y") * mouseSensitivity;
+
+		pitch = Mathf.Clamp (pitch, pitchMinMax.x, pitchMinMax.y);
+
+		currentRotation = Vector3.SmoothDamp (currentRotation, new Vector3 (pitch, yaw), ref rotationSmoothVelocity, rotationSmoothTime);
+		//Vector3 targetRotation = new Vector3 (pitch, yaw);
+		transform.eulerAngles = currentRotation;
+
+        if (isZoomed == false)
+        {
+            transform.position = target.position - transform.forward * dsFromTarget;
+        }
+    }
+}
---
+++ art-sword-online/Assets/Scripts/MovementInput.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementInput : MonoBehaviour {
+
+	public float InputX;
+	public float InputZ;
+	public Vector3 desiredMoveDirection;
+	public bool blockRotationPlayer;
+	public float desiredRotationSpeed;
+	public Animator anim;
+	public float Speed;
+	public float allowPlayerRotation;
+	private Camera cam;
+	public CharacterController controller;
+	public bool isGrounded;
+	private float verticalVel;
+	private Vector3 moveVector;
+    public float gravity;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+
+    // Use this for initialization
+    void Start () {
+		anim = GetComponent<Animator> ();
+		cam = Camera.main;
+		controller = this.GetComponent<CharacterController> ();
+
+	}
+
+    void Update () {
+		InputMagnitude ();
+
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        if (Physics.Raycast(transform.position, -Vector3.up, 1))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+		moveVector = new Vector3 (0, verticalVel, 0);
+		controller.Move (moveVector);
+
+	}
+
+
+
+    void PlayerMoveAndRotation(){
+		InputX = Input.GetAxis ("Horizontal");
+		InputZ = Input.GetAxis ("Vertical");
+
+			var camera = Camera.main;
+			var forward = cam.transform.forward;
+			var right = cam.transform.right;
+
+		forward.y = 0f;
+		right.y = 0f;
+
+		forward.Normalize ();
+		right.Normalize ();
+
+		desiredMoveDirection = forward * InputZ + right * InputX;
+
+		if (blockRotationPlayer == false) {
+			transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (desiredMoveDirection), desiredRotationSpeed);
+		}
+	}
+
+	void InputMagnitude() {
+		//calculate Input Vectors
+		InputX = Input.GetAxis("Horizontal");
+		InputZ = Input.GetAxis("Vertical");
+
+		anim.SetFloat ("InputZ", InputZ, 0.0f, Time.deltaTime * 2f);
+		anim.SetFloat ("InputX", InputX, 0.0f, Time.deltaTime * 2f);
+
+		//Calculate the Input Magnitude
+		Speed = new Vector2(InputX, InputZ).sqrMagnitude;
+
+		//Phisically move Player
+		if (Speed > allowPlayerRotation) {
+			anim.SetFloat ("InputMagnitude", Speed, 0.0f, Time.deltaTime);
+			PlayerMoveAndRotation ();
+		} else if (Speed < allowPlayerRotation) {
+			anim.SetFloat ("InputMagnitude", Speed, 0.0f, Time.deltaTime);
+		}
+	}
+}
