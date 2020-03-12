5acc59a0243f7d5a0c21245ac67ae0aadcd1ec15
joao matheus bueno <jmatheus_05@hotmail.com>
2020-03-12 13:43:04 -0300

    Atualizando Hammer

--- art-sword-online/Assets/Warrior Pack Bundle 3 FREE/Hammer Warrior Mecanim Animation Pack/Base_Project/Scripts/Character.cs
+++ art-sword-online/Assets/Warrior Pack Bundle 3 FREE/Hammer Warrior Mecanim Animation Pack/Base_Project/Scripts/Character.cs
@@ -9,10 +9,12 @@ public class Character : MonoBehaviour
     public float gravity = 30.0f;
     public Vector3 moveDir = Vector3.zero;
     public CharacterController control;
-   
+    public Animator anim;
+    //Animator anim = new Animator();
+
     void Start()
     {
-        
+
     }
 
     void Update()
@@ -30,5 +32,19 @@ public class Character : MonoBehaviour
         float rotation = Input.GetAxis("Horizontal3D") * velrotation;
         rotation *= Time.deltaTime;
         transform.Rotate(0, rotation, 0);
+
+        if (control.isGrounded)
+        {
+            if(moveDir.y != 0)
+            {
+                anim.SetBool("Stop", false);
+                anim.SetBool("Run", true);
+            }
+            else
+            {
+                anim.SetBool("Stop", true);
+                anim.SetBool("Run", false);
+            }
+        }
     }
 }
